4b957eae6e8070fe0c5ce13b477b8738c5ff667d
Gauthier Leonard <gauthier@e36knots.com>
2023-03-28 10:23:43 +0200

    fix: social card image name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
         defaultMode: "dark",
       },
       // Replace with your project's social card
-      image: "img/ash-social-card.jpg",
+      image: "img/ash-social-card.png",
       navbar: {
         title: "Ash",
         logo: {
