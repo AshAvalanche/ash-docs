0cc00e341ca1f749d806ef458047af5011c2504c
Léo Schoukroun <leopaul36@yahoo.fr>
2023-04-14 09:43:38 +0200

    feat: split Homepage components

---
+++ src/components/Homepage/Header/index.tsx
@@ -0,0 +1,32 @@
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import clsx from "clsx";
+import React from "react";
+
+import styles from "./styles.module.css";
+
+export default function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/toolkit"
+          >
+            Get started
+          </Link>
+          {/* <Link
+            className="button button--secondary button--lg"
+            to="/docs/toolkit"
+          >
+            Read the docs
+          </Link> */}
+        </div>
+      </div>
+    </header>
+  );
+}
