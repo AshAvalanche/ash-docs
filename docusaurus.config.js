47a7afffccba84c74b8f91fc9424ab0c6d0bb6e7
Léo Schoukroun <leo@e36knots.com>
2023-07-26 15:09:17 +0200

    fix: baseurl to ash.center

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/ash-logo.svg",
 
   // Set the production url of your site here
-  url: "https://docs.ash.center/",
+  url: "https://ash.center/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
