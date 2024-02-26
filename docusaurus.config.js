c49fd9f3262017425209636b36743c16ab51e806
gaetan cabaret <gaetan@e36knots.com>
2024-02-26 16:42:02 +0100

    fix(search): remove de from languages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,9 +26,9 @@ const config = {
 
   // Search bar plugin
   plugins: [[ require.resolve('docusaurus-lunr-search'), {
-    languages: ['en', 'de'] // language codes
+    languages: ['en'] // language codes
   }]],
-  
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
